7607a8a5a2d7ecee7e7d41000c108844afcb729b
Joachim Hagege <joachim.hagege@spironet.com>
2024-07-04 14:53:45 +0300

    Refactor: Optimize collision detection methods using binary search

--- collision_detector/time_interval_collision_detector.py
+++ collision_detector/time_interval_collision_detector.py
@@ -1,21 +1,21 @@
 from typing import List, Tuple, Dict, Optional
 
+import bisect
 
-class ResourceLock:
+from collision_detector.interface import CollisionDetector
+
+
+class IntervalConflictDetector(CollisionDetector):
     def __init__(self):
         self.locks: Dict[str, List[Tuple[int, int]]] = {}
 
     def add_lock(self, resource_id: str, start_time: int, end_time: int):
         if resource_id not in self.locks:
             self.locks[resource_id] = [(start_time, end_time)]
-            return
-
-        intervals = self.locks[resource_id]
-        # Find the correct position to insert the new interval to keep the list sorted
-        i = 0
-        while i < len(intervals) and intervals[i][0] < start_time:
-            i += 1
-        intervals.insert(i, (start_time, end_time))
+        else:
+            intervals = self.locks[resource_id]
+            i = bisect.bisect_left(intervals, (start_time, end_time))
+            intervals.insert(i, (start_time, end_time))
 
     def find_first_collision(self, resource_id: str) -> Optional[Tuple[int, int]]:
         intervals = self.locks.get(resource_id, [])
@@ -26,9 +26,9 @@ class ResourceLock:
 
     def is_locked(self, resource_id: str, t: int) -> bool:
         intervals = self.locks.get(resource_id, [])
-        for start, end in intervals:
-            if start <= t < end:
-                return True
+        i = bisect.bisect_right(intervals, (t, float('inf'))) - 1
+        if i >= 0 and intervals[i][0] <= t < intervals[i][1]:
+            return True
         return False
 
     def has_collision_at(self, resource_id: str, t: int) -> bool:
